d305abab4945eb605acc27d52458fe2bc8c58d87
Thomas Jetzinger <thomas.jetzinger@amv-networks.com>
2018-03-27 17:41:45 +0200

    Add public constructor for Identity.

--- AMVKit/Sources/Models/Identity/Identity.swift
+++ AMVKit/Sources/Models/Identity/Identity.swift
@@ -4,4 +4,10 @@ public struct Identity: Codable {
     public let privateKey: String
     public let publicKey: String
     public let deviceSerialNumber: String
+    
+    public init(privateKey: String, publicKey: String, deviceSerialNumber: String) {
+        self.privateKey = privateKey
+        self.publicKey = publicKey
+        self.deviceSerialNumber = deviceSerialNumber
+    }
 }
