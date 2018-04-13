fe5fbd577bdb1898166518fe9fa904cb1efcede6
Thomas Jetzinger <thomas.jetzinger@amv-networks.com>
2018-04-13 08:49:35 +0200

    Add unit test for isValid

--- AMVKitTests/AMVKitTests.swift
+++ AMVKitTests/AMVKitTests.swift
@@ -3,19 +3,25 @@ import XCTest
 
 class AMVKitTests: XCTestCase {
     
-    // valid from 2018-04-05 10:06 until 2018-04-13 10:04
-    let accessCertificateJSON = """
-    {
-        "id": "1",
-        "name" : "Test",
-        "device_access_certificate": "ASPnnwX4LULu8n0gGxdWPBinq6a8B50U0er+PIQah2cYsxtb0dJaEmrzlnFRWGx/qe58uT/07HXiLYePyJebUaFABbi9q2eVoMSncPRcchtQPBIEBQgGEgQNCAQHEAAfCAAAQMoTmg6ldPxnK3zIAuQuXeceVlqJtECtLBqy4Ff3Y3JZ3t7aiFuK5UC6HfndIXHcwSPV1pwU9kyvrxhNyTFMB4k=",
-        "vehicle_access_certificate": "xKdw9FxyG1A8iVbu7cxJyeTmoo4jWbng39rhc8rgGUHXtkxtiWGITEH2XV3/Ox+pqOLs4rjxLqAZ9x2ExCqzn6KSWglEDLkbHwEj558F+C1C7hIEBQgGEgQNCAQHEAAfCAAAQEf1JaqPBCmM2KbzyUtZ3Z5kUkkyuQ1UmP8ov+LwJhEapGmXYKeTAcCxUd2TzuxgIBrNeMsecvvj59XzydGwYjE="
-    }
-    """.data(using: .utf8, allowLossyConversion: true)!
+    var accessCertificateJSON: Data?
+    let certificateUUID = UUID().uuidString
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        createTestAccessCertificate()
+    }
+    
+    func createTestAccessCertificate() {
+        // valid from 2018-04-05 10:06 until 2018-04-13 10:04
+        accessCertificateJSON =
+            """
+                {
+                "id": "\(certificateUUID)",
+                "name" : "Test",
+                "device_access_certificate": "ASPnnwX4LULu8n0gGxdWPBinq6a8B50U0er+PIQah2cYsxtb0dJaEmrzlnFRWGx/qe58uT/07HXiLYePyJebUaFABbi9q2eVoMSncPRcchtQPBIEBQgGEgQNCAQHEAAfCAAAQMoTmg6ldPxnK3zIAuQuXeceVlqJtECtLBqy4Ff3Y3JZ3t7aiFuK5UC6HfndIXHcwSPV1pwU9kyvrxhNyTFMB4k=",
+                "vehicle_access_certificate": "xKdw9FxyG1A8iVbu7cxJyeTmoo4jWbng39rhc8rgGUHXtkxtiWGITEH2XV3/Ox+pqOLs4rjxLqAZ9x2ExCqzn6KSWglEDLkbHwEj558F+C1C7hIEBQgGEgQNCAQHEAAfCAAAQEf1JaqPBCmM2KbzyUtZ3Z5kUkkyuQ1UmP8ov+LwJhEapGmXYKeTAcCxUd2TzuxgIBrNeMsecvvj59XzydGwYjE="
+                }
+                """.data(using: .utf8, allowLossyConversion: true)!
     }
     
     override func tearDown() {
@@ -23,8 +29,13 @@ class AMVKitTests: XCTestCase {
         super.tearDown()
     }
     
+    func test_itShouldCreateValidAccessCertificate() {
+        createTestAccessCertificate()
+    }
+    
     func testIfCertificatesStartAndEndTimeIsValid() {
         if let accessCertificate = getTestAccessCertificate() {
+            
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yy-MM-dd hh:mm"
         
@@ -53,7 +64,7 @@ class AMVKitTests: XCTestCase {
     func getTestAccessCertificate() -> AccessCertificate? {
         do {
             let decoder = JSONDecoder()
-            return try decoder.decode(AccessCertificate.self, from: accessCertificateJSON)
+            return try decoder.decode(AccessCertificate.self, from: accessCertificateJSON!)
         } catch {
             XCTFail("Failed to create access certificate, error: \(error)")
             return nil
